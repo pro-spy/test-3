037ffa1ca02f7acb84d7cbaf6c9bff445f095ca3
Mr. Nobody <anonymousprospy@gmail.com>
2026-03-14 15:27:31 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 double d= 22.1;
 boolean = false
+float m = 10.3f;
 	public static void main(String[] args) {
 		SpringApplication.run(Test3Application.class, args);
 	}
