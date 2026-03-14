e9dbbdf1214249b107a04b6dde8cae77e1bb72d5
Mr. Nobody <anonymousprospy@gmail.com>
2026-03-14 14:47:41 +0530

    Add variable x with initial value of 2

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test3Application {
+ int x = 2
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test3Application.class, args);
