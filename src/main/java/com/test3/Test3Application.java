07973ef5cf915281b172ecc188d24e9da858b3aa
pro-spy <anonymousprospy@gmail.com>
2026-03-14 15:10:35 +0530

    first commit

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test3Application {
+boolean = false
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test3Application.class, args);
